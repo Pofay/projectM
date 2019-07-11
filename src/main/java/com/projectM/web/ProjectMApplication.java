71bf40a99abe617aa7c4cacd62f20faa59d76cf9
pofay <gianCarlo.gilos@outlook.com>
2019-07-11 15:15:34 +0800

    Setup base runnable project with example properties

---
+++ src/main/java/com/projectM/web/ProjectMApplication.java
@@ -0,0 +1,15 @@
+package com.projectM.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMApplication.class, args);
+	}
+
+	
+
+}
